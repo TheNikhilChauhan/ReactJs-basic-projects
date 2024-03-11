ad07ceac7455eb411e524a223b2e4377333d124b
Nikhil <ace.nikchauhan@gmail.com>
2024-03-11 15:02:28 +0530

    added useRef(): playPause_video

--- 01.React Basics/useRef/src/PlayPause.jsx
+++ 01.React Basics/useRef/src/PlayPause.jsx
@@ -1,13 +1,29 @@
-import React, { useRef, useState } from "react";
+import { useRef, useState } from "react";
 
 const PlayPause = () => {
   const [isPlaying, setIsPlaying] = useState(false);
   const ref = useRef();
 
+  function handleClick() {
+    const nextIsPlaying = !isPlaying;
+    setIsPlaying(nextIsPlaying);
+
+    if (nextIsPlaying) {
+      ref.current.play();
+    } else {
+      ref.current.pause();
+    }
+  }
+
   return (
     <div>
-      <button>{isPlaying ? "Pause" : "Play"}</button>
-      <video>
+      <button onClick={handleClick}>{isPlaying ? "Pause" : "Play"}</button>
+      <video
+        ref={ref}
+        width="250"
+        onPlay={() => setIsPlaying(true)}
+        onPause={() => setIsPlaying(false)}
+      >
         <source
           src="https://interactive-examples.mdn.mozilla.net/media/cc0-videos/flower.mp4"
           type="video/mp4"
